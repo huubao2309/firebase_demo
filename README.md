325441d757d42f8465f873cf3b5c0ed8ad0d2326
Huu Bao Nguyen <huubao2309@gmail.com>
2020-12-24 13:57:07 +0700

    add Firebase for project and handle to foreground

--- README.md
+++ README.md
@@ -1,7 +1,67 @@
-# Firebase Demo
+# Firebase
 
-- How to get SHA-1
-https://stackoverflow.com/questions/51845559/generate-sha-1-for-flutter-app
+## Guide setup Firebase on Flutter Android:
 
-- Continute Step 4:
-https://firebase.google.com/docs/flutter/setup?platform=android
+- Android: https://console.firebase.google.com/
+
+## 1. Create a Firebase project:
+
+https://firebase.google.com/docs/flutter/setup?platform=android#create-firebase-project
+
+**Note**: Get SHA-1 on Android Studio (https://stackoverflow.com/questions/51845559/generate-sha-1-for-flutter-app)
+
+## 2. Register your app with Firebase:
+
+https://firebase.google.com/docs/flutter/setup?platform=android#register-app
+
+## 3. Add a Firebase configguration file:
+
+https://firebase.google.com/docs/flutter/setup?platform=android#add-config-file
+
+## 4. Add FlutterFire plugins:
+
+https://firebase.google.com/docs/flutter/setup?platform=android#add-flutterfire-plugins
+
+```dart
+	firebase_analytics: ^6.2.0
+	firebase_messaging: ^7.0.3
+	firebase_core: ^0.5.2
+```
+
+**Reference**: https://github.com/rajayogan/flutter-pushnotifications-basics
+
+## 5. Get Firebase Token:
+
+```dart
+	final FirebaseMessaging _messaging = FirebaseMessaging();
+	//....
+	
+	  @override
+	void initState() {
+		super.initState();
+
+		_messaging.getToken().then((token) {
+		  print(token);
+		});
+	}
+```
+
+## 6. Receive message to Firebase (Foreground):
+
+```dart
+	_messaging.configure(
+      onMessage: (message) async {
+        print('onMessage Firebase: $message');
+      },
+      // onBackgroundMessage: myBackgroundMessageHandler,
+      onLaunch: (message) async {
+        print('onLaunch Firebase: $message');
+      },
+      onResume: (message) async {
+        print('onResume Firebase: $message');
+      },
+    );
+```
+
+## 7. Receive message to Firebase (Background)
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_email_sender/flutter_email_sender.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -8,19 +15,121 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Firebase Analytics Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
       home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  MyHomePage();
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  _MyHomePageState();
+
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+
+  String messageStr = 'No message';
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _firebaseNotification();
+  }
+
+  void _firebaseNotification() {
+    _firebaseMessaging.configure(
+      onMessage: (message) async {
+        print('onMessage Firebase: $message');
+        setState(() {
+          messageStr = message.toString();
+        });
+      },
+      // onBackgroundMessage: myBackgroundMessageHandler,
+      onLaunch: (message) async {
+        print('onLaunch Firebase: $message');
+        setState(() {
+          messageStr = message.toString();
+        });
+      },
+      onResume: (message) async {
+        print('onResume Firebase: $message');
+        setState(() {
+          messageStr = message.toString();
+        });
+      },
+    );
+
+    // // ignore: cascade_invocations
+    // _firebaseMessaging
+    //     // ignore: cascade_invocations
+    //     .requestNotificationPermissions(const IosNotificationSettings(sound: true, badge: true, alert: true, provisional: true));
+
+    // _firebaseMessaging.onIosSettingsRegistered.listen((settings) {
+    //   print('Settings registered: $settings');
+    // });
+
+    _firebaseMessaging.getToken().then((token) async {
+      assert(token != null, 'Token is null');
+      if (token == null) {
+        setState(() {
+          token = 'Token is null';
+        });
+      } else if (messageStr == 'No message') {
+        // Send Email
+        await sendEmail(token);
+        setState(() {
+          messageStr = token;
+        });
+      }
+
+      print('Push Messaging token: $token');
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 200,
-      height: 200,
-      color: Colors.blue,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Demo Firebase'),
+      ),
+      body: Center(
+        child: Text(messageStr),
+      ),
     );
+  }
+}
+
+Future<void> sendEmail(String token) async {
+  final Email email = Email(
+    body: 'Token: $token',
+    subject: 'Token gui ve',
+    recipients: ['huubao2309@gmail.com'],
+    isHTML: false,
+  );
+
+  await FlutterEmailSender.send(email);
+}
+
+// ignore: missing_return
+Future<dynamic> myBackgroundMessageHandler(Map<String, dynamic> message) async {
+  if (message.containsKey('data')) {
+    final dynamic data = message['data'];
+    await sendEmail('Data: ${message.toString()}');
+    print('$data');
+  }
+
+  if (message.containsKey('notification')) {
+    final dynamic notification = message['notification'];
+    await sendEmail('Notification: ${message.toString()}');
+    print('$notification');
   }
 }
